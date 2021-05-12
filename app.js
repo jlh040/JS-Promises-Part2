72e7b2bed57347ac57473da16df43458e626e853
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-12 11:00:28 -0700

    a button is now on the page and the container divs are centered

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
 
 let url = 'https://deckofcardsapi.com/api/deck/fsrp01s1rj6y/draw/?count=1';
 
-axios.get(url)
-    .then(resp => {
-        console.log(`${resp.data.cards[0].value} of ${resp.data.cards[0].suit}`)
-        return axios.get(url)
-    })
-    .then(resp => {
-        console.log(`${resp.data.cards[0].value} of ${resp.data.cards[0].suit}`)
-    });
+// axios.get(url)
+//     .then(resp => {
+//         console.log(`${resp.data.cards[0].value} of ${resp.data.cards[0].suit}`)
+//         return axios.get(url)
+//     })
+//     .then(resp => {
+//         console.log(`${resp.data.cards[0].value} of ${resp.data.cards[0].suit}`)
+//         return axios.get(url)
+//     })
+//     .then(resp => {
+//         console.log('IMAGE URL', resp.data.cards[0].image)
+//         $('.card-container').append(`
+//         <img src=${resp.data.cards[0].image}>`)
+//     })
+    
+$('button').click(function(e) {
+    console.log(1)
+})
