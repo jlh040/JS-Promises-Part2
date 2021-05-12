fba0fefff0cb7e4a2965c3fe9beb7e2307d695a3
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-12 12:10:18 -0700

    the page now gets a new deck everytime it loads

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 // fsrp01s1rj6y deck id
-let url = 'https://deckofcardsapi.com/api/deck/fsrp01s1rj6y/draw/?count=1';
+let deckID;
+let deckURL;
+
+$(function() {
+    axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        .then(resp => {
+            deckID = resp.data.deck_id;
+            return deckID 
+        })
+        .then(deckID => {
+            deckURL = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`
+        })
+})
     
 $('button').click(function(e) {
     getCardImageURL()
 })
 
 function getCardImageURL() {
-    axios.get(url)
+    axios.get(deckURL)
         .then(resp => {
             imageURL = resp.data.cards[0].image;
             putCardOnPage(imageURL)
@@ -32,5 +44,5 @@ function randomRotateValue() {
 }
 
 function randomPixel() {
-    return Math.floor(Math.random() * 5);
+    return Math.floor(Math.random() * 50);
 }
